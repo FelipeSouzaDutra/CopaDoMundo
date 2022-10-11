1f9daeb09956b0d705dfec4b429b9e711494126c
joao-botrel <gleiserjosealves@gmail.com>
2022-10-11 18:03:30 -0300

    Seção da história do Brasil na copa finalizada.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,11 +2,10 @@ import React from 'react'
 
 const Footer = () => {
   return (
-   <footer className=" bg-slate-700 shadow-md">
+   <footer className="bg-independence shadow-md">
      <div className=' flex items-center justify-center'>
-        <span className='text-sm font-bold text-white p-4 '>
-            Desenvolvido por <a className="hover:text-black hover:underline  focus:bg-black focus:underline transition-all" href="https://www.instagram.com/joao.botrel/" target="_blank"> João Botrel </a> e 
-            <a className=" hover:text-black hover:underline  focus:bg-black focus:underline transition-all" href="https://www.instagram.com/dutra3678/" target="_blank">Felipe Dutra</a> 2022
+        <span className='text-sm font-bold text-light-steel-blue p-4 '>
+            Desenvolvido por <a className="hover:text-alabaster transition-all" href="https://www.instagram.com/joao.botrel/" target="_blank"> João Botrel </a> e <a className="hover:text-alabaster transition-all" href="https://www.instagram.com/dutra3678/" target="_blank">Felipe Dutra</a> 2022
         </span>
      </div>
    </footer>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,36 @@ module.exports = {
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      "lincoln-green": "#14591d",
+      "sap-green": "#57822b",
+      "olive-drab": "#789632",
+      "citron": "#99aa38",
+      "light-yellow": "#e1e289",
+      "independence": "#4d5061",
+      "glaucous": "#5c80bc",
+      "light-steel-blue": "#a3b7cd",
+      "light-gray": "#c6d2d6",
+      "platinum": "#d8e0da",
+      "alabaster": "#e1e1dc",
+    },
+
     fontFamily:{
-      "sans": [" Product Sans Regular","Helvetica","Arial"]
+      "sans": ["Product Sans Regular","Helvetica","Arial"]
      },
 
 
-    extend: {},
+    extend: {
+      spacing: {
+        '97': '80rem',
+        '100': '32rem',
+      },
+
+      backgroundImage: {
+        'time-abraçado': "url('/img/time-junto.jpg')",
+      }
+      
+    },
   },
   plugins: [],
 }
